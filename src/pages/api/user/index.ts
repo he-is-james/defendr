af461dd3ddce8ab4e715fd4a30643f2674aad3db
danielkao14 <daniel.kao196@gmail.com>
2023-04-22 14:35:08 -0700

    styling for sign up

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -3,35 +3,38 @@ import { db } from '../../../../firebase'
 import { User, ErrorMessage } from '../../../interfaces/index'
 
 export default async function userHandler(
-    req: NextApiRequest,
-    res: NextApiResponse<User | ErrorMessage>
-  ) {
-    const { query, method } = req   
-    const { email } = query 
+  req: NextApiRequest,
+  res: NextApiResponse<User | ErrorMessage>
+) {
+  const { query, method } = req
+  const { email } = query
 
-    switch (method) {
-        case 'GET':
-            try {
-                const querySnapshot = await db.collection('users').where('email', '==', email).get()
-                if (querySnapshot.empty) {
-                    res.status(404).json({ message: 'User not found'})
-                    return
-                }
-                const documentSnapshot = querySnapshot.docs[0]
-                const user = documentSnapshot.data() as User;
-                res.status(200).json(user)
-            } catch (err) {
-                res.status(404).json({ message: 'Get User failed'})
-            }
+  switch (method) {
+    case 'GET':
+      try {
+        const querySnapshot = await db
+          .collection('users')
+          .where('email', '==', email)
+          .get()
+        if (querySnapshot.empty) {
+          res.status(404).json({ message: 'User not found' })
+          return
+        }
+        const documentSnapshot = querySnapshot.docs[0]
+        const user = documentSnapshot.data() as User
+        res.status(200).json(user)
+      } catch (err) {
+        res.status(404).json({ message: 'Get User failed' })
+      }
 
-            break
-        case 'POST':
-            try {
-                console.log('here')
-                const newUser: User = req.body
-                await db.collection('users').add(newUser)
-            } catch (err) {
-                res.status(404).json({ message: 'Add User failed'})
-            }
-    }
+      break
+    case 'POST':
+      try {
+        console.log('here')
+        const newUser: User = req.body
+        await db.collection('users').add(newUser)
+      } catch (err) {
+        res.status(404).json({ message: 'Add User failed' })
+      }
   }
+}
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 // import { db, auth } from '../../../firebase'
 import { auth } from '../../../firebase'
 import { useRouter } from 'next/router'
+import TextField from '@mui/material/TextField'
+import { Typography, Button } from '@mui/material'
+import Link from 'next/link'
+
 const SignUpPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+
   const router = useRouter()
   const handleSignUp = async () => {
+    if (confirmPassword !== password) {
+      throw new Error('Passwords do not match')
+    }
     try {
       await auth.createUserWithEmailAndPassword(email, password)
       router.push('/login')
@@ -16,29 +28,156 @@ const SignUpPage = () => {
   }
 
   return (
-    <div>
-      <h1>Sign Up Page</h1>
-      <form>
-        <label>
-          Email:
-          <input
+    <div className="flex h-screen justify-center items-center bg-gradient-to-br from-red-400 to-red-200 h-screen w-screen">
+      <div>
+        <div className="text-center mb-4">
+          <Typography
+            variant="h2"
+            className="flex justify-center"
+            fontFamily="Berkshire Swash"
+          >
+            Defendr
+          </Typography>
+        </div>
+        <div className="flex space-x-5 mb-4">
+          <div className="mb-4 rounded-md bg-white pt-1 pd-1 w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <TextField
+              id="outlined-basic"
+              label="First"
+              variant="standard"
+              required
+              sx={{
+                ml: 1,
+                borderRadius: '6px',
+                '& .MuiInputLabel-root': {
+                  marginTop: '-4px',
+                },
+              }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              type="first"
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div className="mb-4 rounded-md bg-white pt-1 pd-1 w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <TextField
+              id="outlined-basic"
+              label="Last"
+              variant="standard"
+              required
+              sx={{
+                ml: 1,
+                borderRadius: '6px',
+                '& .MuiInputLabel-root': {
+                  marginTop: '-4px',
+                },
+              }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              type="last"
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="mb-4 rounded-md bg-white pt-1">
+          <TextField
+            id="outlined-basic"
+            label="email"
+            variant="standard"
+            required
+            sx={{
+              ml: 1,
+              borderRadius: '6px',
+              '& .MuiInputLabel-root': {
+                marginTop: '-4px',
+              },
+            }}
+            InputProps={{
+              disableUnderline: true,
+            }}
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            fullWidth
           />
-        </label>
-        <label>
-          Password:
-          <input
+        </div>
+        <div className="mb-4 rounded-md bg-white pt-1">
+          <TextField
+            id="outlined-basic"
+            label="password"
+            variant="standard"
+            required
+            sx={{
+              ml: 1,
+              borderRadius: '6px',
+              '& .MuiInputLabel-root': {
+                marginTop: '-4px',
+              },
+            }}
+            InputProps={{
+              disableUnderline: true,
+            }}
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            fullWidth
           />
-        </label>
-        <button type="button" onClick={handleSignUp}>
-          Sign Up
-        </button>
-      </form>
+        </div>
+        <div className="mb-4 rounded-md bg-white pt-1">
+          <TextField
+            id="outlined-basic"
+            label="confirm password"
+            variant="standard"
+            required
+            sx={{
+              ml: 1,
+              borderRadius: '6px',
+              '& .MuiInputLabel-root': {
+                marginTop: '-4px',
+              },
+            }}
+            InputProps={{
+              disableUnderline: true,
+            }}
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            fullWidth
+          />
+        </div>
+        <div className="py-2 rounded ">
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: '#8B5B33',
+              color: '#FFFFFF',
+              '&:hover': {
+                bgcolor: '#6C3F23',
+              },
+            }}
+            className="w-full bg-amber-900"
+            onClick={handleSignUp}
+          >
+            Sign up
+          </Button>
+        </div>
+        <div className="flex justify-center">
+          <Typography>
+            {' '}
+            Already have an account?{' '}
+            <Link
+              href="/login"
+              className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            >
+              Sign in
+            </Link>
+          </Typography>
+        </div>
+      </div>
     </div>
   )
 }
