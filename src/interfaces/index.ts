3c55e536f673dfd1fd9e54cfaa9c05c812d3c265
James He <he.is.james7@gmail.com>
2023-04-22 19:05:11 -0700

    switched sign up to api

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,31 +1,31 @@
 export type User = {
-    email: string,
-    firstName: string,
-    lastName: string,
-    hearts: number[],
-    progress: number[]
+  email: string
+  firstName: string
+  lastName: string
+  hearts: number[]
+  progress: number[]
 } | null
 
 // TODO: replace the question marks once more finalized
 export type Persona = {
-    name: string,
-    pronouns: string,
-    category: string,
-    scammer: boolean,
-    type: string,
-    background: string,
-    properties: string[],
-    // image: string,
-    bond: number,
-    prompts: string[],
-    responses: string[],
-    clues: string[]
-    userChoices: string[],
-    blocked: number,
-    matched: number,
-    misidentified: number
+  name: string
+  pronouns: string
+  category: string
+  scammer: boolean
+  type: string
+  background: string
+  properties: string[]
+  // image: string,
+  bond: number
+  prompts: string[]
+  responses: string[]
+  clues: string[]
+  userChoices: string[]
+  blocked: number
+  matched: number
+  misidentified: number
 } | null
 
 export type ErrorMessage = {
-    message: string
+  message: string
 }
--- src/pages/activity/personaMessage.tsx
+++ src/pages/activity/personaMessage.tsx
@@ -1,51 +1,53 @@
 interface Props {
-    name: string,
-    pronouns: string,
-    background: string,
-    properties: string[],
-    bond: number,
-    prompts: string[],
-    responses: string[]
+  name: string
+  pronouns: string
+  background: string
+  properties: string[]
+  bond: number
+  prompts: string[]
+  responses: string[]
 }
 
 export default function PersonaMessage({
-    name, pronouns, background, properties, bond, prompts, responses
+  name,
+  pronouns,
+  background,
+  properties,
+  bond,
+  prompts,
+  responses,
 }: Props) {
-    return(
+  return (
+    <div>
+      <div>
+        <div>{name}</div>
+        <div>{pronouns}</div>
+        <div>TODO: press here later for instructions, new component</div>
+      </div>
+      <div>
+        <div>How you know them</div>
+        <div>{background}</div>
+      </div>
+      <div>
         <div>
-            <div>
-                <div>{name}</div>
-                <div>{pronouns}</div>
-                <div>
-                    TODO: press here later for instructions, new component
-                </div>
-            </div>
-            <div>
-                <div>How you know them</div>
-                <div>{background}</div>
-            </div>
-            <div>
-                <div>
-                    <div>Age: {properties[0]}</div>
-                    <div>Height: {properties[1]}</div>
-                    <div>Occupation: {properties[2]}</div>
-                    <div>Location: {properties[3]}</div>
-                </div>
-                <div>
-                    TODO: hardcode image url here
-                </div>
-            </div>
-            <div>
-                <div>Bond: {bond}</div>
-                <div>
-                    <div>{prompts[0]}</div>
-                    <div>{responses[0]}</div>
-                </div>
-            </div>
-            <div>
-                <div>{prompts[1]}</div>
-                <div>{responses[1]}</div>
-            </div>
+          <div>Age: {properties[0]}</div>
+          <div>Height: {properties[1]}</div>
+          <div>Occupation: {properties[2]}</div>
+          <div>Location: {properties[3]}</div>
         </div>
-    )
+        <div>TODO: hardcode image url here</div>
+      </div>
+      <div>
+        <div>Bond: {bond}</div>
+        <div>
+          <div>{prompts[0]}</div>
+          <div>{responses[0]}</div>
+        </div>
+      </div>
+      <div>
+        <div>{prompts[1]}</div>
+        <div>{responses[1]}</div>
+      </div>
+    </div>
+  )
 }
