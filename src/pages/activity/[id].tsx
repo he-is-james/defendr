71b5118c077f1dcde25f44c971556a9e2ecef382
James He <he.is.james7@gmail.com>
2023-04-23 06:28:55 -0700

    forgot to add

--- src/pages/activity/[id].tsx
+++ src/pages/activity/[id].tsx
@@ -2,18 +2,17 @@ import Navbar from '@/components/navbar'
 import { Persona } from '@/interfaces'
 import axios from 'axios'
 import { useRouter } from 'next/router'
-import PersonaMessage from './personamessage'
+// import PersonaMessage from './personamessage'
 import { useEffect, useState } from 'react'
 import Actionbar from '@/components/actionbar'
 import { Card } from '@mui/material'
 import Chat from './chat'
-import InstructionsPopup from './instruction'
+// import InstructionsPopup from './instruction'
+import Image from 'next/image'
+import james from './../../../public/james.svg'
+import angela from './../../../public/angela.svg'
 
 export default function Activity() {
-  const [open, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-  // TODO: add the redux functionality here
   const router = useRouter()
   const { id } = router.query
   const personaId = Array.isArray(id) ? id[0] : id
@@ -23,42 +22,42 @@ export default function Activity() {
   const [count, setCount] = useState(1)
   const [persona, setPersona] = useState<Persona | null>(null)
 
-  const createPersona = async () => {
-    try {
-      const newPersona: Persona = {
-        name: 'Angela Ling',
-        pronouns: 'he/him',
-        category: personaId || '',
-        scammer: true,
-        type: 'message',
-        background:
-          'James is someone you were close to during your college years, but you never really had a chance to get to know her well. You occasionally run into each other at alumni events, but you both have different lives now and rarely have a chance to catch up.',
-        properties: ['21', `4'11`, 'Student', 'Los Angeles'],
-        bond: 4,
-        prompts: ['In my free time', 'What would I do with 1 million dollars'],
-        responses: [
-          'Walking around a forest with a bucket hat',
-          'A cure for cancer',
-        ],
-        clues: [
-          'Hey sexy',
-          'Hey sexy1',
-          'Hey sexy2',
-          'Hey sexy3',
-          'Hey sexy4',
-          'Hey sexy5',
-        ],
-        userChoices: ['choice1', 'choice2', 'choice3', 'choice4', 'choice5'],
-        blocked: 0,
-        matched: 0,
-        misidentified: 0,
-      }
-      const response = await axios.post(`/api/personas/${id}3`, newPersona)
-      console.log(response)
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  // const createPersona = async () => {
+  //   try {
+  //     const newPersona: Persona = {
+  //       name: 'Angela Ling',
+  //       pronouns: 'he/him',
+  //       category: personaId || '',
+  //       scammer: true,
+  //       type: 'message',
+  //       background:
+  //         'James is someone you were close to during your college years, but you never really had a chance to get to know her well. You occasionally run into each other at alumni events, but you both have different lives now and rarely have a chance to catch up.',
+  //       properties: ['21', `4'11`, 'Student', 'Los Angeles'],
+  //       bond: 4,
+  //       prompts: ['In my free time', 'What would I do with 1 million dollars'],
+  //       responses: [
+  //         'Walking around a forest with a bucket hat',
+  //         'A cure for cancer',
+  //       ],
+  //       clues: [
+  //         'Hey sexy',
+  //         'Hey sexy1',
+  //         'Hey sexy2',
+  //         'Hey sexy3',
+  //         'Hey sexy4',
+  //         'Hey sexy5',
+  //       ],
+  //       userChoices: ['choice1', 'choice2', 'choice3', 'choice4', 'choice5'],
+  //       blocked: 0,
+  //       matched: 0,
+  //       misidentified: 0,
+  //     }
+  //     const response = await axios.post(`/api/personas/${id}3`, newPersona)
+  //     console.log(response)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   const getPersona = async () => {
     try {
@@ -73,50 +72,72 @@ export default function Activity() {
     getPersona()
   }, [count])
 
-  return (
-    <div>
-      <Navbar />
+  if (count === 1) {
+    return (
       <div>
-        <button
-          onClick={() => {
-            createPersona()
-          }}
-        >
-          Test create persona
-        </button>
+        <Navbar />
+        {/* <div>
+          <button
+            onClick={() => {
+              createPersona()
+            }}
+          >
+            Test create persona
+          </button>
+        </div> */}
+        <div className="flex flex-row">
+          <div className="w-1/3">
+            {/* {persona ? ( */}
+            <Image alt="james profile" src={james} />
+            {/* ) : (
+              <PersonaMessage
+                name={persona.name}
+                pronouns={persona.pronouns}
+                background={persona.background}
+                properties={persona.properties}
+                bond={persona.bond}
+                prompts={persona.prompts}
+                responses={persona.responses}
+              />
+            )} */}
+          </div>
+          <div className="w-2/3">
+            <Chat
+              clues={persona?.clues || []}
+              userChoices={persona?.userChoices || []}
+            />
+          </div>
+        </div>
+        <Card>
+          <Actionbar id={url} scammer={true} count={count} setCount={setCount} />
+        </Card>
+        {/* <button onClick={handleOpen}>Open Modal</button> */}
+        {/* <InstructionsPopup isOpen={open} onClose={handleClose}></InstructionsPopup> */}
       </div>
+    )
+  } else if (count == 2) {
+    return (
       <div>
-        {persona ? (
-          <PersonaMessage
-            name={persona.name}
-            pronouns={persona.pronouns}
-            background={persona.background}
-            properties={persona.properties}
-            bond={persona.bond}
-            prompts={persona.prompts}
-            responses={persona.responses}
-          />
-        ) : (
-          <></>
-        )}
+        <Navbar />
+        <div className="flex flex-row">
+          <div className="w-1/3">
+            <Image alt="angela profile" src={angela} />
+          </div>
+          <div className="w-2/3">
+            <Chat
+              clues={persona?.clues || []}
+              userChoices={persona?.userChoices || []}
+            />
+          </div>
+        </div>
+        <Card>
+          <Actionbar id={url} scammer={false} count={count} setCount={setCount} />
+        </Card>
+        {/* <button onClick={handleOpen}>Open Modal</button>
+        <InstructionsPopup isOpen={open} onClose={handleClose}></InstructionsPopup> */}
       </div>
-      <Card>
-        {/* if the chat is messagesimple vs messagedifficult */}
-        <Chat
-          clues={persona?.clues || []}
-          userChoices={persona?.userChoices || []}
-        />
-      </Card>
-      <Card>
-        <Actionbar
-          id={url}
-          scammer={persona?.scammer || false}
-          count={count}
-          setCount={setCount}
-        />
-      </Card>
-      <button onClick={handleOpen}>Open Modal</button>
-      <InstructionsPopup isOpen={open} onClose={handleClose}></InstructionsPopup>
-    </div>
-  )
+    )
+  } else {
+    router.push('/home')
+  }
 }
--- src/pages/activity/personaMessage.tsx
+++ src/pages/activity/personaMessage.tsx
@@ -1,4 +1,7 @@
-import { Button, Typography, Grid, Paper } from '@mui/material'
+import { Typography } from '@mui/material'
+import Image from 'next/image'
+import instructions from './../../../public/instructions.svg'
+
 interface Props {
   name: string
   pronouns: string
@@ -19,57 +22,46 @@ export default function PersonaMessage({
   responses,
 }: Props) {
   return (
-    <div>
+    <div className="p-4">
       <div className="flex flex-row">
-        <Typography variant="h1" fontFamily="Berkshire Swash">
-          {name}
-        </Typography>
-        <Typography variant="h3" fontFamily="Berkshire Swash">
-          {pronouns}
-        </Typography>
-        <Button>instruction</Button>
-      </div>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        rowSpacing={1}
-        columnSpacing={2}
-      >
-        <Grid item xs={6}>
-          <Paper>
-            <div>How you know them</div>
-            <div>{background}</div>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>
-            <div>
-              <div>Age: {properties[0]}</div>
-              <div>Height: {properties[1]}</div>
-              <div>Occupation: {properties[2]}</div>
-              <div>Location: {properties[3]}</div>
-            </div>
-            <div>CHANGE CHANGE CHANGE CHANGE to image</div>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <div>
-            <Paper>Bond: {bond}</Paper>
-            <Paper>
-              <div>{prompts[0]}</div>
-              <div>{responses[0]}</div>
-            </Paper>
+        <div className="pr-2">
+          <Typography variant="h3" fontFamily="Berkshire Swash">
+            {name}
+          </Typography>
+        </div>
+        <div className="">
+          <Typography fontFamily="Open Sans">{pronouns}</Typography>
+        </div>
+        <div className="flex justify-end">
+          <div className="">
+            <button>
+              <Image alt="instructions" src={instructions} />
+            </button>
           </div>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>
-            <div>{prompts[1]}</div>
-            <div>{responses[1]}</div>
-          </Paper>
-        </Grid>
-      </Grid>
+        </div>
+      </div>
+      <div>
+        <div>How you know them</div>
+        <div>{background}</div>
+      </div>
+      <div>
+        <div>Age: {properties[0]}</div>
+        <div>Height: {properties[1]}</div>
+        <div>Occupation: {properties[2]}</div>
+        <div>Location: {properties[3]}</div>
+      </div>
+      <div>CHANGE CHANGE CHANGE CHANGE to image</div>
+      <div>
+        <div>Bond: {bond}</div>
+        <div>
+          <div>{prompts[0]}</div>
+          <div>{responses[0]}</div>
+        </div>
+      </div>
+      <div>
+        <div>{prompts[1]}</div>
+        <div>{responses[1]}</div>
+      </div>
     </div>
   )
 }
