5cb3eb6e2e491e2e3a9344d303e3426673acf3a1
danielkao14 <daniel.kao196@gmail.com>
2023-04-23 07:24:02 -0700

    lesson page build in progress

--- src/pages/lesson/[id].tsx
+++ src/pages/lesson/[id].tsx
@@ -1,7 +1,9 @@
 import Navbar from '@/components/navbar'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-
+import { IconButton, Typography } from '@mui/material'
+import lessonsvg from '/public/lesson.svg'
+import Image from 'next/image'
 export default function Lesson() {
   const router = useRouter()
   const { id } = router.query
@@ -13,20 +15,26 @@ export default function Lesson() {
   return (
     <div>
       <Navbar />
-      <div>
-        <div>Lesson: {lesson}</div>
-        <div>Lorem ipsum</div>
-      </div>
-      <div>
-        <div>Common Types of Scams</div>
-        <div>Lorem ipsum</div>
-      </div>
-      <div>
-        <div>Let&apos;s Practice</div>
+      <div className="  m-32 flex flex-col">
+        <Image alt="lesson page" src={lessonsvg}></Image>
         <div>
-          <button>
-            <Link href={`/activity/${id}`}>Begin</Link>
-          </button>
+          <div className="flex flex-row">
+            <Typography
+              className="text-left"
+              variant="h3"
+              fontFamily="Berkshire Swash"
+            >
+              Let&apos;s Practice
+            </Typography>
+            {/* <IconButton>
+              <InfoIcon></InfoIcon>
+            </IconButton> */}
+          </div>
+          <div>
+            <button>
+              <Link href={`/activity/${id}`}>Begin</Link>
+            </button>
+          </div>
         </div>
       </div>
     </div>
